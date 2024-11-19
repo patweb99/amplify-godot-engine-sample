6387d886be6ea2dec25ee9505e016c4d6ac165bd
Frédéric Nowak <96630035+fnowak77@users.noreply.github.com>
2024-11-19 16:04:12 +0100

    Add export scripts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,17 @@
+import iam from 'aws-cdk-lib/aws-iam';
+import s3 from 'aws-cdk-lib/aws-s3';
 import { defineBackend } from '@aws-amplify/backend';
+import { storage } from './storage/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
+    storage: storage
 });
+/*backend.storage.resources.bucket.addToResourcePolicy(new iam.PolicyStatement({
+    effect: iam.Effect.ALLOW,
+    principals: [new iam.ServicePrincipal("amplify.amazonaws.com")],
+    actions: ["s3:PutObject"],
+    resources: [backend.storage.resources.bucket.arnForObjects("*")]
+}))*/
