1cbd825a74362e93c735dd6ea36ae2c44546966c
Frédéric Nowak <fredenow@amazon.com>
2024-11-20 17:04:20 +0100

    Sync art, package.json and more

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,4 +1,3 @@
-import * as cognito from 'aws-cdk-lib/aws-cognito';
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
